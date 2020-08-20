cc9d7020a216aeaee9312db73e1d45ca861bffe9
Aleksa <lukicaleksa04@gmail.com>
2020-08-20 12:55:58 +0200

    Form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from "./material.module";
 import { LoginDialogComponent } from './login-dialog/login-dialog.component';
 import { RegistrationDialogComponent } from './registration-dialog/registration-dialog.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,6 +20,8 @@ import { RegistrationDialogComponent } from './registration-dialog/registration-
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
+    ReactiveFormsModule,
+    
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/registration-dialog/registration-dialog.component.ts
+++ src/app/registration-dialog/registration-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration-dialog',
@@ -9,7 +10,22 @@ export class RegistrationDialogComponent implements OnInit {
 
   constructor() { }
 
+  userInfoForm = new FormGroup({
+    full_name: new FormControl("", Validators.required),
+    email: new FormControl("", Validators.required)
+  })
+
+  userForm = new FormGroup({
+    username: new FormControl("", Validators.required),
+    password: new FormControl("", Validators.required)
+  })
+
+
   ngOnInit() {
+
   }
 
+  register() {
+
+  }
 }
